4f1e4d02668fe06dd634bd9c9645c8088d937640
shaeredal <shaeredal@gmail.com>
2016-05-11 23:42:59 +0300

    parsing comixes to json

--- itransition-project/itransition-project/Scripts/_references.js
+++ itransition-project/itransition-project/Scripts/_references.js
@@ -17,6 +17,7 @@
 /// <reference path="angular-touch.min.js" />
 /// <reference path="app/addcomix.js" />
 /// <reference path="app/admindragdrop.js" />
+/// <reference path="app/jsonscripts.js" />
 /// <reference path="app/templates_script.js" />
 /// <reference path="app/userinfo.js" />
 /// <reference path="bootstrap.js" />
